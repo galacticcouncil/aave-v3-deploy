cb4a93a9a0b42b3b191a6a7f08feaac1b6f8a0b7
mrq <mrq@mrq.cz>
2025-04-10 13:52:00 +0200

    lil refactor

--- contracts/USDOracleAdapter.sol
+++ contracts/USDOracleAdapter.sol
@@ -1,43 +1,38 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.10;
 
+import "@aave/periphery-v3/contracts/misc/interfaces/IEACAggregatorProxy.sol";
 import {AggregatorInterface} from '@aave/core-v3/contracts/dependencies/chainlink/AggregatorInterface.sol';
 
-contract  USDOracleAdapter {
-    AggregatorInterface _assetToXOracle;
-    AggregatorInterface _XToUsdOracle;
+contract  USDOracleAdapter is IEACAggregatorProxy {
 
-    event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
-    event NewRound(uint256 indexed roundId, address indexed startedBy);
+    uint8 public constant decimals = 8;
 
-    error NotImplemented();
+    AggregatorInterface public assetToXOracle;
+    AggregatorInterface public XToUsdOracle;
 
-    constructor(address assetToXOracle, address XToUsdOracle) {
-        _assetToXOracle = AggregatorInterface(assetToXOracle);
-        _XToUsdOracle = AggregatorInterface(XToUsdOracle);
-    }
-
-    function decimals() external view returns (uint8) {
-        return 8;
+    constructor(address _assetToXOracle, address _XToUsdOracle) {
+        assetToXOracle = AggregatorInterface(_assetToXOracle);
+        XToUsdOracle = AggregatorInterface(_XToUsdOracle);
     }
 
     function latestAnswer() external view returns (int256) {
-        return int256((uint256(_assetToXOracle.latestAnswer()) * uint256(_XToUsdOracle.latestAnswer())) /uint256(10)**8);
+        return int256((uint256(assetToXOracle.latestAnswer()) * uint256(XToUsdOracle.latestAnswer())) / uint256(10) ** decimals);
     }
 
     function latestTimestamp() external view returns (uint256) {
-        revert NotImplemented();
+        return block.timestamp;
     }
 
     function latestRound() external view returns (uint256) {
-        revert NotImplemented();
+        return 0;
     }
 
     function getAnswer(uint256 roundId) external view returns (int256) {
-        return int256((uint256(_assetToXOracle.latestAnswer()) * uint256(_XToUsdOracle.latestAnswer())) /uint256(10)**8);
+        return this.latestAnswer();
     }
 
     function getTimestamp(uint256 roundId) external view returns (uint256) {
-        revert NotImplemented();
-    } 
+        return 0;
+    }
 }
