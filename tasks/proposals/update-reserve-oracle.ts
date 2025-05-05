2a54246d7a7969e3d60b6c60b56b09d4df8c1e43
mrq <mrq@mrq.cz>
2025-05-05 12:01:50 +0200

    increased supply cap for vdot

--- tasks/proposals/update-reserve-oracle.ts
+++ tasks/proposals/update-reserve-oracle.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import {
   generateProposalV2,
-  rootEvmCall,
+  aaveManagerCall,
 } from "../../helpers/hydration-proposal.js";
 import { task } from "hardhat/config";
 import { getBatch, clearBatch } from "../../helpers/transaction-batch";
@@ -15,41 +15,39 @@ import {
   getACLManager,
 } from "../../helpers";
 
-task(`update-reserve-oracle`, ``)
-  .addParam("reserve", "symbol of the reserve")
-  .setAction(async function (
-    { reserve, batch = false }: { reserve: string; batch: boolean },
-    hre
-  ) {
-    const networkId = FORK ? FORK : hre.network.name;
-    const admin = POOL_ADMIN[networkId];
-    const signer = await hre.ethers.getSigner(admin);
-    const poolAddressesProvider = await getPoolAddressesProvider();
-    const aclManager = (
-      await getACLManager(await poolAddressesProvider.getACLManager())
-    ).connect(signer);
-    const isPoolAdmin = await aclManager.isPoolAdmin(admin);
+task(`bifrosto-vdot-oracle`, ``).setAction(async function (_, hre) {
+  const networkId = FORK ? FORK : hre.network.name;
+  const admin = POOL_ADMIN[networkId];
+  const signer = await hre.ethers.getSigner(admin);
+  const poolAddressesProvider = await getPoolAddressesProvider();
+  const aclManager = (
+    await getACLManager(await poolAddressesProvider.getACLManager())
+  ).connect(signer);
+  const isPoolAdmin = await aclManager.isPoolAdmin(admin);
 
-    if (!isPoolAdmin) {
-      consoleerror(chalk.red(`not pool admin ${admin}`));
-      exit(1);
-    }
+  if (!isPoolAdmin) {
+    console.error(chalk.red(`not pool admin ${admin}`));
+    exit(1);
+  }
 
-    const txs = [];
-    await hre.run("set-reserve-oracle", {
-      symbol: reserve,
-      batch: true,
-    });
-    for await (const el of getBatch()) {
-      el.from = admin;
-      txs.push(await rootEvmCall(el));
-    }
-    clearBatch();
-
-    let preimage = await generateProposalV2(txs, false);
-    const decoder = new ProposalDecoder(hre);
-    await decoder.init();
-    console.log("submit preimages:");
-    console.log(preimage.toHex());
-    decoder.printTree(decoder.transformCall(preimage.toHuman()));
+  await hre.run("set-reserve-oracle", {
+    symbol: "VDOT",
+    batch: true,
   });
+
+  await hre.run("review-supply-caps", {
+    fix: true,
+    batch: true,
+  });
+
+  const txs = await Promise.all(
+    getBatch().map((tx) => aaveManagerCall({ ...tx, from: admin }))
+  );
+
+  let preimage = await generateProposalV2(txs, false);
+  const decoder = new ProposalDecoder(hre);
+  await decoder.init();
+  console.log("submit preimages:");
+  console.log(preimage.toHex());
+  decoder.printTree(decoder.transformCall(preimage.toHuman()));
+});
