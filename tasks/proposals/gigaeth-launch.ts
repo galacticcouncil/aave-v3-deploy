13c06533fb7cd474a4369fa55467197c2e1e0431
martinfridrich <mrtnfridrich@gmail.com>
2025-06-06 15:22:03 +0200

    gigaety-launch: fix ETH oracle setup and add ETH e-mode setup

--- tasks/proposals/gigaeth-launch.ts
+++ tasks/proposals/gigaeth-launch.ts
@@ -221,6 +221,14 @@ task(`gigaeth-launch`, ``).setAction(async function (_, hre) {
   }
   clearBatch();
 
+  console.log("setup ETH emode");
+  await hre.run("review-e-mode", { fix: true, batch: true, name: "StableEMode" });
+  for await (const el of getBatch()) {
+    el.from = admin;
+    txs.push(await rootEvmCall(el));
+  }
+  clearBatch();
+
   console.log("---------> create stableswap pool with pegs");
   const wstEthEthOracle = chainlinkConf.WSTETH_ETH;
   if (!wstEthEthOracle) {
