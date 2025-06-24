1923c6d35dded82361a2db5a43e43cf0a164c8eb
mrq <mrq@mrq.cz>
2025-06-24 10:57:26 +0200

    fixed build

--- helpers/types.ts
+++ helpers/types.ts
@@ -546,6 +546,7 @@ export interface IBaseConfiguration {
   };
   RateStrategies: IStrategy;
   USDOracleAdapter?: any;
+  OraclesAggregator?: any;
 }
 
 export interface ICommonConfiguration extends IBaseConfiguration {}
