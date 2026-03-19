7b1a17424d2360c928baf52819bf331be5b37be6
Martin Hloska <martin.hloska@gmail.com>
2026-03-19 15:48:37 +0100

    gigahdx support

--- markets/zombie/index.ts
+++ markets/zombie/index.ts
@@ -25,8 +25,6 @@ import {
   strategyHUSDe,
   strategyPAXG,
   strategyPRIME,
-  strategySOL,
-  strategyGSOL,
   strategySTHDX,
 } from "./reservesConfigs";
 import { tokenAddress } from "./helpers";
@@ -66,8 +64,6 @@ export const HydrationConfig: IAaveConfiguration = {
     "2-POOL-HUSDE": strategyHUSDe,
     PAXG: strategyPAXG,
     PRIME: strategyPRIME,
-    SOL: strategySOL,
-    "2-POOL-GSOL": strategyGSOL,
     STHDX: strategySTHDX,
   },
   ReserveAssets: {
@@ -89,8 +85,6 @@ export const HydrationConfig: IAaveConfiguration = {
       "2-POOL-HUSDE": tokenAddress(113),
       PAXG: tokenAddress(39),
       PRIME: tokenAddress(43),
-      SOL: tokenAddress(1000752),
-      "2-POOL-GSOL": tokenAddress(90001),
       STHDX: tokenAddress(670),
     },
     [eHydrationNetwork.nice]: {
@@ -148,14 +142,6 @@ export const HydrationConfig: IAaveConfiguration = {
       label: "ETH",
       assets: ["ETH", "2-Pool-GETH"],
     },
-    SolEMode: {
-      id: "4",
-      ltv: "8000",
-      liquidationThreshold: "8500",
-      liquidationBonus: "10450",
-      label: "SOL correlated",
-      assets: ["SOL", "2-Pool-GSOL"],
-    },
   },
   ChainlinkAggregator: {
     [eHydrationNetwork.hydration]: {
@@ -180,11 +166,6 @@ export const HydrationConfig: IAaveConfiguration = {
       "2-POOL-HUSDE": "0x00000102737461626c657377000000de00000071", // HOLLAR(222) / 2-POOL-HUSDe(113) 10 min. stablesw
       PAXG: "0x8fB61B8E81C2f17695F14A136C98b0C4013bc105",
       PRIME: "0xDEe587cC569bf1FcBdcD6d1472031d225f34C307",
-      // GIGASOL oracles
-      SOL: "0x2FAA73BCC0115b9F67d2f36E53738B7FF95f0D2C", // DIA SOL/USD oracle
-      "2-POOL-GSOL": "0xCD3648A48378cBDa915f6be0A30073b76593Ed9A",
-      JITOSOL_SOL: "0x5B29bceaCBD1c37FD4A2c32a052b63813ed0D4b8",
-      STHDX: "0xbd1108369553bfFBAaa1BA5C8D07a8131EB92F10", // deploy-USDOracleAdapter.ts
       STHDX: "0x202df3eDac2775b857ee2f61A3569731E53eC713", // deploy-USDOracleAdapter.ts
     },
     [eHydrationNetwork.zombie]: {
@@ -203,12 +184,6 @@ export const HydrationConfig: IAaveConfiguration = {
       WSTETH: "0x52bBB0BC38C42D60b24EBF0C617E8218D2aB6d36", //TODO: waiting on DIA
       "2-POOL-GETH": "0x493f00bA516E55e5CA932f55CeB6b5c4b6E4257F", //TODO: deploy USDOracleAdapter and use real address
       WSTETH_ETH: "0x493f00bA516E55e5CA932f55CeB6b5c4b6E4257F", //TODO: deploy OraclesAggregator and use real address
-    },
-    [eHydrationNetwork.zombie]: {
-      // GIGASOL oracles for zombie testing
-      SOL: "0x2FAA73BCC0115b9F67d2f36E53738B7FF95f0D2C", // DIA SOL/USD oracle (same as mainnet fork)
-      "2-POOL-GSOL": "0xCD3648A48378cBDa915f6be0A30073b76593Ed9A", // USDOracleAdapter TODO: REPLACE BEFORE CREATING GIGASOL PROPOSAL
-      JITOSOL_SOL: "0x5B29bceaCBD1c37FD4A2c32a052b63813ed0D4b8", // ManagedOracle (jitoSOL/SOL) TODO: REPLACE BEFORE CREATING GIGASOL PROPOSAL
     },
   },
   IncentivesConfig: {
@@ -317,18 +292,6 @@ export const HydrationConfig: IAaveConfiguration = {
         assetToX: "0x00000102737461626c657377000003ea00000067", //hydration's chainlink precompile, stableswap 10min., aUSDT(1002)/3-POOL(103)
         xToUSD: "0x8b0DDfB8F56690eAde9ECa23a7d90E153C268d5B", // DIA USDT/USD oracle
       },
-      // GIGASOL - stableswap price oracle for 2-POOL-GSOL
-      "2-POOL-GSOL": {
-        assetToX: "0x00000102737461626c657377000003f100015f91", // hydration's chainlink precompile, stableswap 10min., aSOL(1009)/gSOLs(90001)
-        xToUSD: "0x2FAA73BCC0115b9F67d2f36E53738B7FF95f0D2C", // DIA SOL/USD oracle
-      },
-    },
-    [eHydrationNetwork.zombie]: {
-      // GIGASOL - stableswap price oracle for 2-POOL-GSOL (same config as mainnet)
-      "2-POOL-GSOL": {
-        assetToX: "0x00000102737461626c657377000003f100015f91", // hydration's chainlink precompile, stableswap 10min., aSOL(1009)/gSOLs(90001)
-        xToUSD: "0x2FAA73BCC0115b9F67d2f36E53738B7FF95f0D2C", // DIA SOL/USD oracle
-      },
       STHDX: {
         assetToX: "0x0000010267696761686478730000029e00000000", // gigahdxs source: stHDX(670)/HDX(0) TenMinutes
         xToUSD: "0xea63e594ee00590938E856F2134E6C792bA92d13", // DIA HDX/USD oracle
@@ -347,7 +310,6 @@ export const HydrationConfig: IAaveConfiguration = {
         srcAssetToX: "0x52bBB0BC38C42D60b24EBF0C617E8218D2aB6d36", //wstETH -> USD
         destAssetToX: "0x1AF549Fe19A9B73D094173C41e18BF7F357F594b", //ETH -> USD
       },
-      // NOTE: JITOSOL_SOL uses ManagedOracle pattern (like wstETH), not OraclesAggregator
     },
   },
 };
